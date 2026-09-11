6fe100d0db70570d6131c14f7c29cfa819bd590f
ojonasalves <31973601+ojonasalves@users.noreply.github.com>
2026-09-10 22:25:16 -0300

    Change database password in config.php
    Updated database password for security reasons.

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ const APP_NAME = 'Rede Solidária';
 const DB_HOST = 'localhost';
 const DB_NAME = 'jonasa20_rede_solidaria';
 const DB_USER = 'jonasa20_rede_admin';
-const DB_PASS = '@Jonas123e4@';
+const DB_PASS = 'SENHA_AQUI';
 
 date_default_timezone_set('America/Sao_Paulo');
 require_once __DIR__ . '/database.php';
